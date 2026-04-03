5631d76d2c52adb11047ea6b2fecc3aba33315e4
n12333204-prateek <prateek.shrishailuppin@connect.qut.edu.au>
2026-04-03 20:14:11 +1000

    Added ci.yml file
    Added ci.yml file, test case file and updated live baseURL

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5001', // local
-  //baseURL: 'http://3.26.96.188:5001', // live
+  // baseURL: 'http://localhost:5001', // local
+  baseURL: 'http://54.206.146.40:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
